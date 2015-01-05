69269bc0590875929d572ea237d20906d880f288
Andrea Ferrandi <ferrandi.andrea@gmail.com>
2015-01-05 20:26:58 +0100

    fixed error with 'where ' constraints

--- src/dbtarzan/db/Table.scala
+++ src/dbtarzan/db/Table.scala
@@ -8,11 +8,15 @@ import dbtarzan.messages.{QueryRows, QueryForeignKeys, TableId}
 class Table private (description : TableDescription, columns : Fields, foreignConstraint : Option[ForeignKeyCriteria], additionalConstraint : Option[Constraint]) {
 	val sql = buildSql()
 
-	def buildSql() : String = { 
+	def buildSql() : String = {
+		def buildConstraints(constraints : List[String]) = if(!constraints.isEmpty)   
+			constraints.mkString(" WHERE (\n", ") AND (\n", ")")
+		else 
+			""
+
 		var foreignClosure = foreignConstraint.map(ForeignKeyTextBuilder.buildClause(_))
-		"select * from " + description.name +
-			foreignClosure.map(" WHERE (\n"+_+")").getOrElse("") +
-			additionalConstraint.map(" AND (\n"+_.text+")").getOrElse("")
+		val constraints = List(foreignClosure, additionalConstraint.map(_.text)).flatten
+		"select * from " + description.name + buildConstraints(constraints)
 	}
 
 
