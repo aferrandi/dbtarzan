0a0138a78e7428bf701a4b0bcac0b11ba38b3165
Andrea Ferrandi <ferrandi.andrea@gmail.com>
2015-01-27 21:59:56 +0100

    Fixes the following bug:
    foreign keys order #7
    error lines in wrong orders #6
    Error reading table in Oracle database #4

--- src/dbtarzan/db/ForeignKeyTextBuilder.scala
+++ src/dbtarzan/db/ForeignKeyTextBuilder.scala
@@ -21,17 +21,21 @@ class ForeignKeyTextBuilder(criteria : ForeignKeyCriteria) {
 	private def buildFieldText(fieldWithValue : FieldWithValue) = {
 		val field = fieldWithValue.field
 		mapColumnTypes.get(field) match {
-			case Some(fieldType) => field + "=" + buildFieldValueText(fieldWithValue.value, fieldType) 
-			case None => throw new Exception("field "+fieldWithValue.field+" not found in column types "+mapColumnTypes.keys) 
+			case Some(fieldType) => buildFieldValueText(fieldWithValue, fieldType) 
+			case None => throw new Exception("field "+field+" not found in column types "+mapColumnTypes.keys) 
 		}
-					
 	}
 
-	private def buildFieldValueText(fieldValue : String, fieldType : FieldType) = 
-		if(fieldType == FieldType.STRING)
-			"'" + fieldValue + "'"
+	private def buildFieldValueText(fieldWithValue : FieldWithValue, fieldType : FieldType) = {
+		val field = fieldWithValue.field
+		val fieldValue = fieldWithValue.value
+		if(fieldValue == null)
+			field + " IS NULL"
+		else if(fieldType == FieldType.STRING)
+			field + "='" + fieldValue + "'"
 		else 
-			fieldValue.toString
+			field + "=" +  fieldValue.toString
+	}
 
 	private def buildRowText(fkRow : FKRow) =
 		fkRow.values.map(fkValue => buildFieldText(fkValue)).mkString("(", " AND ", ")")
