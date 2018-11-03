ea8514163e9832f7b4cf9bf87ed4cce8ddf1870a
andrea <ferrandi.andrea@gmail.com>
2018-11-03 14:49:33 +0100

    added test on column names

--- test/db/IntegrationTest..scala
+++ test/db/IntegrationTest..scala
@@ -15,6 +15,22 @@ class IntegrationTest extends FlatSpec with BeforeAndAfter {
   	assert(List("LAPTOP", "PC", "PRINTER", "PRODUCT" ) === tableNames.tableNames)
   }
   
+
+  "columnNames of LAPTOP" should "give a sorted list of the table names" in {
+    val metadataLoader = new BasicMetadataLoader(None, connection.getMetaData())
+    val columnNames = metadataLoader.columnNames("LAPTOP")
+  	assert(
+      List(
+        Field("CODE", FieldType.INT), 
+        Field("MODEL", FieldType.STRING), 
+        Field("SPEED", FieldType.INT), 
+        Field("RAM", FieldType.INT), 
+        Field("HD", FieldType.FLOAT),
+        Field("PRICE", FieldType.FLOAT), 
+        Field("SCREEN",FieldType.INT)
+        ) === columnNames.fields)
+  }
+
   before {
     Class.forName("org.h2.Driver")
     connection = DriverManager.getConnection( "jdbc:h2:mem:;INIT=runscript from 'classpath:sampledb/computer.sql';", "sa", "" )
