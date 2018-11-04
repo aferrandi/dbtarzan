e357b21e51e3c15eb9018c02ae2002e32845201a
andrea <ferrandi.andrea@gmail.com>
2018-11-04 19:30:01 +0100

    changed the test to use SqlBuilder

--- test/db/SqlBuilderTest.scala
+++ test/db/SqlBuilderTest.scala
@@ -2,64 +2,55 @@ package dbtarzan.db
 
 import org.scalatest.FlatSpec
 
-class DBTableTest extends FlatSpec {
+class SqlBuilderTest extends FlatSpec {
   "a simple table" should "give a simple query" in {
-    val table = DBTable.build(
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         None,
         None,
         None,
         noneApplier()
       )
-    val sql = table.buildSql()
   	assert("SELECT * FROM customer" === sql)
   }
 
   "a simple table with delimiters" should "give a query with delimiters" in {
-      val applier = QueryAttributesApplier.from(QueryAttributes(Some(IdentifierDelimiters('[', ']')), Some("TST")))
-    val table = DBTable.build(
+    val applier = QueryAttributesApplier.from(QueryAttributes(Some(IdentifierDelimiters('[', ']')), Some("TST")))
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         None,
         None,
         None,
         applier
       )
-    val sql = table.buildSql()
     assert("SELECT * FROM [TST].[customer]" === sql)
   }
 
   "a table with foreign criteria" should "give a query with a where clause" in {
-    val table = DBTable.build(
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         Some(buildForeignKeyCriteria()),
         None,
         None,
         noneApplier()
       )
-    val sql = table.buildSql()
     assert("SELECT * FROM customer WHERE (\n(name='John' AND age=23))" === sql)
   }
 
   "a table with additional filter" should "give a query with a where clause" in {
-    val table = DBTable.build(
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         None,
         Some(Filter("name = 'john'")),
         None,
         noneApplier()
       )
-    val sql = table.buildSql()
     assert("SELECT * FROM customer WHERE (\nname = 'john')" === sql)
   }
 
   "a table with additional order by columns" should "give a query with an order by clause" in {
-    val table = DBTable.build(
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         None,
         None,
         Some(OrderByFields(List(
@@ -68,20 +59,17 @@ class DBTableTest extends FlatSpec {
           ))),
         noneApplier()
       )
-    val sql = table.buildSql()
     assert("SELECT * FROM customer ORDER BY name ASC, age DESC" === sql)
   }
 
   "a table with empty order by columns list" should "give a simple query" in {
-    val table = DBTable.build(
+    val sql = SqlBuilder.buildSql(
         buildDescription(),
-        Fields(buildColumns()),
         None,
         None,
         Some(OrderByFields(List.empty[OrderByField])),
         noneApplier()
       )
-    val sql = table.buildSql()
     assert("SELECT * FROM customer" === sql)
   }
 
@@ -90,7 +78,6 @@ class DBTableTest extends FlatSpec {
     val rows = List(buildRow("John", "23"))
     ForeignKeyCriteria(rows, buildColumns())
   }
-
 
   private def buildNameColumn() = Field("name",  FieldType.STRING)
   private def buildAgeColumn() = Field("age",  FieldType.INT)
