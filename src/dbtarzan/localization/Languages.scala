4a3605d648190a6005820bc67d9f831e57fa192e
andrea <ferrandi.andrea@gmail.com>
2019-01-20 22:44:49 +0100

    added spanish

--- src/dbtarzan/localization/Languages.scala
+++ src/dbtarzan/localization/Languages.scala
@@ -5,8 +5,9 @@ case class Language(language : String)
 object Languages {
     val ENGLISH = Language("english")
     val ITALIAN = Language("italian")
+    val SPANISH = Language("spanish")
 
-    val languages = List(ENGLISH, ITALIAN)
+    val languages = List(ENGLISH, ITALIAN, SPANISH)
 
     val default = ENGLISH
 }
--- src/dbtarzan/localization/Localizations.scala
+++ src/dbtarzan/localization/Localizations.scala
@@ -6,6 +6,7 @@ object Localizations
         language match {
             case Languages.ENGLISH => new English()
             case Languages.ITALIAN => new Italian()
+            case Languages.SPANISH => new Spanish()
             case _  => new English()
         }  
 }
