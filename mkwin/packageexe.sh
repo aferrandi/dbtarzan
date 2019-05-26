9b74df98b83c415b722ba57b5901aa1832741fea
andrea <ferrandi.andrea@gmail.com>
2019-05-26 14:31:47 +0200

    changed to compile to java 11 and javafx 11

--- mkwin/packageexe.sh
+++ mkwin/packageexe.sh
@@ -4,7 +4,7 @@ WINDIR=$ROOTDIR/mkwin
 VERSION=$2
 echo "ROOTDIR $ROOTDIR WINDIR $WINDIR VERSION $VERSION"
 cp $WINDIR/launch4j_config.mod $WINDIR/launch4j_config.xml
-JARFILE="$ROOTDIR/target/scala-2.12/dbtarzan-assembly-$VERSION.jar"
+JARFILE="$ROOTDIR/prjwin/target/scala-2.12/dbtarzan-assembly-$VERSION.jar"
 OUTFILE="$WINDIR/dbtarzan_$VERSION.exe" 
 ICONFILE="$WINDIR/monkey-face-cartoon.ico"
 JARESCAPED="${JARFILE//\//\\\/}"
