46d3da36aa641f02ef9a3474e5b65395a5bf2365
andrea <ferrandi.andrea@gmail.com>
2019-12-23 14:55:18 +0100

    moved test to the correct directory

--- src/test/scala/dbtarzan/config/connections/ConnectionDataReaderTest.scala
+++ src/test/scala/dbtarzan/config/connections/ConnectionDataReaderTest.scala
@@ -3,14 +3,14 @@ package dbtarzan.config.connections
 import org.scalatest.FlatSpec
 import spray.json._
 import dbtarzan.config.password.Password
+import dbtarzan.db.IdentifierDelimiters
 
 class ConnectionDataReaderTest extends FlatSpec {
    import ConnectionDataJsonProtocol._
-
   "A list of connections" should "be parseable" in {
   	val values= List(
       ConnectionData("oracle.jar", "oracle", "DriverOracle", "jdbc://oracle", None, "giovanni", Password("malagodi"), Some(false), None, None, None, None, None),
-  		ConnectionData("mysql.jar", "mysql", "DriverMysql", "jdbc://mysql", None, "arturo", Password("fedele"), None, None, None, None, None, None)
+  		ConnectionData("mysql.jar", "mysql", "DriverMysql", "jdbc://mysql", None, "arturo", Password("fedele"), None, Some(2), Some(IdentifierDelimiters('"', '"')), Some(300), Some(20), Some("catalog"))
   		)
   	val json = values.toJson.prettyPrint
   	println(json)
