4e7ff617ff43294bab3b3cc9614cef183f5940d6
andrea <ferrandi.andrea@gmail.com>
2019-12-23 15:16:54 +0100

    updated package files to last version of scala

--- macosx/package.sh
+++ macosx/package.sh
@@ -1,16 +1,18 @@
 DIR=$1
 VERSION=$2
+SCALA_VERSION=2.13
+JRE=jre11
 # delete previous versions
 rm -r $DIR/DBTarzan-*.app*
 rm $DIR/dbtarzan-assembly-*.jar
 # download jre
-rm -r $DIR/jre11
-wget -O $DIR/jre11.tar.gz "https://api.adoptopenjdk.net/v2/binary/nightly/openjdk11?openjdk_impl=hotspot&os=mac&arch=x64&release=latest&type=jre"
-tar -zxvf $DIR/jre11.tar.gz -C $DIR
-mv $DIR/jdk* $DIR/jre11
+rm -r $DIR/$JRE
+wget -O $DIR/$JRE.tar.gz "https://api.adoptopenjdk.net/v2/binary/nightly/openjdk11?openjdk_impl=hotspot&os=mac&arch=x64&release=latest&type=jre"
+tar -zxvf $DIR/$JRE.tar.gz -C $DIR
+mv $DIR/jdk* $DIR/$JRE
 # create app
 APP=DBTarzan-$VERSION.app
-cp $DIR/../prjmac/target/scala-2.12/dbtarzan-assembly-$VERSION.jar $DIR
+cp $DIR/../prjmac/target/scala-$SCALA_VERSION/dbtarzan-assembly-$VERSION.jar $DIR
 (cd $DIR; jar2app dbtarzan-assembly-$VERSION.jar -n "DBTarzan-$VERSION" -i monkey-face-cartoon_256x256.icns -j "-DconfigPath=\$HOME/Library/ApplicationSupport/dbtarzan" -e universalJavaApplicationStub -r $DIR/jre11)
 # fix executable (https://github.com/tofi86/universalJavaApplicationStub)
 cp $DIR/universalJavaApplicationStub $DIR/$APP/Contents/MacOS
--- mkwin/packageexe.sh
+++ mkwin/packageexe.sh
@@ -2,9 +2,11 @@
 ROOTDIR=$1
 WINDIR=$ROOTDIR/mkwin
 VERSION=$2
+SCALA_VERSION=2.13
+JRE=jre11
 echo "ROOTDIR $ROOTDIR WINDIR $WINDIR VERSION $VERSION"
 cp $WINDIR/launch4j_config.mod $WINDIR/launch4j_config.xml
-JARFILE="$ROOTDIR/prjwin/target/scala-2.12/dbtarzan-assembly-$VERSION.jar"
+JARFILE="$ROOTDIR/prjwin/target/scala-$SCALA_VERSION/dbtarzan-assembly-$VERSION.jar"
 OUTFILE="$WINDIR/dbtarzan_$VERSION.exe" 
 ICONFILE="$WINDIR/monkey-face-cartoon.ico"
 JARESCAPED="${JARFILE//\//\\\/}"
@@ -14,8 +16,8 @@ sed -i "s/JARFILE/$JARESCAPED/g" $WINDIR/launch4j_config.xml
 sed -i "s/OUTFILE/$OUTESCAPED/g" $WINDIR/launch4j_config.xml
 sed -i "s/ICONFILE/$ICONESCAPED/g" $WINDIR/launch4j_config.xml
 $ROOTDIR/../../bin/launch4j/launch4j $WINDIR/launch4j_config.xml
-rm -r jre11
-wget -O jre11.zip "https://api.adoptopenjdk.net/v2/binary/nightly/openjdk11?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jre"
-unzip jre11.zip
-mv jdk* jre11
+rm -r $JRE
+wget -O $JRE.zip "https://api.adoptopenjdk.net/v2/binary/nightly/openjdk11?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jre"
+unzip $JRE.zip
+mv jdk* $JRE
 makensis -DVERSION=$VERSION $WINDIR/nsis.nsi
