227e41730dca165383d12583bed66576644622a4
andrea <ferrandi.andrea@gmail.com>
2021-11-13 21:33:38 +0100

    the push.sh file contains a comment telling how to use it

--- mksnap/push.sh
+++ mksnap/push.sh
@@ -1,6 +1,6 @@
-#e.g. 1.15
-VERSION=$1
-#e.g. 7
-REVISION=$2
-snapcraft push dbtarzan_$VERSION.0_amd64.snap
+#prj/dbtarzan> mksnap/push.sh $PWD 1.25 18
+ROOTDIR=$1
+VERSION=$2
+REVISION=$3
+snapcraft push $ROOTDIR/mksnap/dbtarzan_$VERSION.0_amd64.snap
 snapcraft release dbtarzan $REVISION stable
