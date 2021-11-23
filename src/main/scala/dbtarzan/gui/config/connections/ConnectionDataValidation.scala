8f7ef9578257f7afc5ba5d0dc87cf0194ce3e07e
andrea <ferrandi.andrea@gmail.com>
2021-11-23 22:31:07 +0100

    #109 validates name in the connection dialog. Test for the validation

--- src/main/scala/dbtarzan/gui/config/connections/ConnectionDataValidation.scala
+++ src/main/scala/dbtarzan/gui/config/connections/ConnectionDataValidation.scala
@@ -10,18 +10,22 @@ object ConnectionDataValidation
 
   def validate(data : ConnectionData) : List[String] =
     List(
-      Some("Empty name").filter(_ => data.name.isEmpty),
-      Some("Name cannot contain spaces").filter(_ => Validation.containsWhitespace(data.name)), 
-      Some("Empty url").filter(_ => data.url.isEmpty),
-      Some("Url cannot contain spaces").filter(_ => Validation.containsWhitespace(data.url)), 
-      Some("Url must be in URL form").filter(_ => !Validation.isValidJdbcURL(data.url)), 
-      Some("Empty driver").filter(_ => data.driver.isEmpty),
-      // Some("Empty user").filter(_ => data.user.isEmpty),
-      // Some("User cannot contain spaces").filter(_ => Validation.containsWhtitespace(data.user)),
-      // Some("Empty password").filter(_ => data.password.isEmpty),
-      // Some("Password cannot contain spaces").filter(_ => Validation.containsWhtitespace(data.password)),
-      Some("Empty jar").filter(_ => data.jar.isEmpty),
-      Some("Jar cannot contain spaces").filter(_ => Validation.containsWhitespace(data.jar)),
-      Some("Max field size should be over "+MAXFIELDSIZE_MIN).filter(_ => !Validation.isMoreThan(data.maxFieldSize, MAXFIELDSIZE_MIN))
-    ).flatten 
+      errorIf("Empty name", _ => data.name.isEmpty),
+      errorIf("Name must contain only letters or digits", _ => !Validation.isDigitsOrLetters(data.name)),
+      errorIf("Empty url", _ => data.url.isEmpty),
+      errorIf("Url cannot contain spaces", _ => Validation.containsWhitespace(data.url)),
+      errorIf("Url must be in URL form", _ => !Validation.isValidJdbcURL(data.url)),
+      errorIf("Empty driver", _ => data.driver.isEmpty),
+      // errorIf("Empty user", _ => data.user.isEmpty),
+      // errorIf("User cannot contain spaces", _ => Validation.containsWhtitespace(data.user)),
+      // errorIf("Empty password", _ => data.password.isEmpty),
+      // errorIf("Password cannot contain spaces", _ => Validation.containsWhtitespace(data.password)),
+      errorIf("Empty jar", _ => data.jar.isEmpty),
+      errorIf("Jar cannot contain spaces", _ => Validation.containsWhitespace(data.jar)),
+      errorIf("Max field size should be over "+MAXFIELDSIZE_MIN, _ => !Validation.isMoreThan(data.maxFieldSize, MAXFIELDSIZE_MIN))
+    ).flatten
+
+  private def errorIf(errorText: String, conditionForError: String => Boolean): Option[String] = {
+    Some(errorText).filter(conditionForError)
+  }
 }
--- src/main/scala/dbtarzan/gui/util/Validation.scala
+++ src/main/scala/dbtarzan/gui/util/Validation.scala
@@ -3,5 +3,6 @@ package dbtarzan.gui.util
 object Validation {
 	def isValidJdbcURL(urlStr : String): Boolean = urlStr.toLowerCase.startsWith("jdbc:")
 	def containsWhitespace(txt : String): Boolean = txt.exists(_.isWhitespace)
-  def isMoreThan(value: Option[Int], min: Int): Boolean = value.map(_ >= min).getOrElse(true)
+  def isMoreThan(value: Option[Int], min: Int): Boolean = value.forall(_ >= min)
+  def isDigitsOrLetters(txt : String): Boolean = txt.forall(c => c.isLetterOrDigit)
 }
