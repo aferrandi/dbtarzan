7a5059e18e3b3405622544e59c01823e427a1db2
andrea <ferrandi.andrea@gmail.com>
2021-11-29 22:58:39 +0100

    #82 added a test to TabsToClose

---
+++ src/test/scala/dbtarzan/gui/tabletabs/TabsToCloseTest.scala
@@ -0,0 +1,26 @@
+package dbtarzan.gui.tabletabs
+
+import dbtarzan.db.{DatabaseId, TableId}
+import dbtarzan.messages.{OriginalQuery, QueryId}
+import org.scalatest.flatspec.AnyFlatSpec
+
+class TabsToCloseTest extends AnyFlatSpec {
+  val table1AId = QueryId(TableId(DatabaseId("database"), "table1"), "0x1232")
+  val table1BId = QueryId(TableId(DatabaseId("database"), "table1"), "0x3232")
+
+  "adding and removing from tabsToCloe" should "find the original element but not the second time" in {
+    val tabsToClose = new TabsToClose()
+    tabsToClose.addToCloseWhenNewTabOpens(table1AId, Some(OriginalQuery(table1BId, true)))
+    val originalQuery = tabsToClose.removeAndGetToCloseWhenNewTabOpens(table1AId)
+    assert(originalQuery.get === OriginalQuery(table1BId, true))
+    val noneQuery = tabsToClose.removeAndGetToCloseWhenNewTabOpens(table1AId)
+    assert(noneQuery === None)
+  }
+
+  "adding nothing to tabsToClose" should "find no original element" in {
+    val tabsToClose = new TabsToClose()
+    tabsToClose.addToCloseWhenNewTabOpens(table1AId, None)
+    val noneQuery = tabsToClose.removeAndGetToCloseWhenNewTabOpens(table1AId)
+    assert(noneQuery === None)
+  }
+}
