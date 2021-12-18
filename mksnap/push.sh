21a749dc409f40cfc3a71356fd574b7f541eba88
andrea <ferrandi.andrea@gmail.com>
2021-12-18 11:58:23 +0100

    snap push was deprecated

--- mksnap/push.sh
+++ mksnap/push.sh
@@ -2,5 +2,4 @@
 ROOTDIR=$1
 VERSION=$2
 REVISION=$3
-snapcraft push $ROOTDIR/mksnap/dbtarzan_$VERSION.0_amd64.snap
-snapcraft release dbtarzan $REVISION stable
+snapcraft upload --release=stable $ROOTDIR/mksnap/dbtarzan_$VERSION.0_amd64.snap
