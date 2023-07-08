1d3caaaa979e66f4d36acaf0a9bc7526ec9ec175
andrea <ferrandi.andrea@gmail.com>
2023-07-09 00:47:08 +0200

    icons in the database list

--- src/main/scala/dbtarzan/gui/DatabaseList.scala
+++ src/main/scala/dbtarzan/gui/DatabaseList.scala
@@ -4,15 +4,19 @@ import dbtarzan.db.DatabaseId
 import dbtarzan.gui.interfaces.{TControlBuilder, TDatabaseList}
 import dbtarzan.gui.util.JFXUtil
 import dbtarzan.localization.Localization
-import dbtarzan.messages.{DatabaseIds, DatabaseIdUtil}
+import dbtarzan.messages.{DatabaseIdUtil, DatabaseIds}
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.Parent
 import scalafx.scene.control._
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.paint.Color
 
 /*	The list of database to choose from*/
 class DatabaseList(localization : Localization) extends TControlBuilder with TDatabaseList {
   private val menuForeignKeyToFile = new MenuItem(localization.buildForeignKeysFile)
   private val buffer = ObservableBuffer.empty[DatabaseId]
+  private val databaseIcon: Image = JFXUtil.loadIcon("database.png")
+  private val compositeIcon: Image = JFXUtil.loadIcon("composite.png")
   private val list = new ListView[DatabaseId](buffer) {
   	SplitPane.setResizableWithParent(this, value = false)
   	contextMenu = new ContextMenu(menuForeignKeyToFile)   
@@ -20,9 +24,24 @@ class DatabaseList(localization : Localization) extends TControlBuilder with TDa
   }
 
   private def buildCell() = new ListCell[DatabaseId] {
-    item.onChange { (_, _, _) => 
-      text.value = Option(item.value).map(DatabaseIdUtil.databaseIdText).getOrElse("")
-    }} 
+    item.onChange { (_, _, value) =>
+      if (value != null) {
+        graphic = new Label {
+          textFill = Color.Black
+          text = Option(item.value).map(DatabaseIdUtil.databaseIdText).getOrElse("")
+          graphic  = new ImageView(iconFromDatabaseId(item.value))
+        }
+      }
+      else
+        graphic = null
+    }
+  }
+
+
+  private def iconFromDatabaseId(databaseId: DatabaseId): Image = databaseId.origin match {
+    case Left(_) => databaseIcon
+    case Right(_) => compositeIcon
+  }
 
   def setDatabaseIds(databaseIds: DatabaseIds) : Unit = {
     println("Got new database list:"+databaseIds)
