0ada984ea6aabafb7773e7a2f3ca11cbeba866e5
andrea <ferrandi.andrea@gmail.com>
2023-09-30 13:24:53 +0200

    fixed password dialog to have 2 columns

--- src/main/scala/dbtarzan/gui/login/PasswordDialog.scala
+++ src/main/scala/dbtarzan/gui/login/PasswordDialog.scala
@@ -4,30 +4,36 @@ import dbtarzan.config.password.Password
 import dbtarzan.db.{LoginPasswords, SimpleDatabaseId}
 import dbtarzan.gui.util.JFXUtil
 import dbtarzan.localization.Localization
-import scalafx.scene.control.{ButtonType, Dialog, PasswordField}
 import scalafx.Includes.*
-import scalafx.scene.layout.{BorderPane, VBox}
+import scalafx.scene.control.{ButtonType, Dialog, Label, PasswordField}
+import scalafx.scene.layout.GridPane
 object PasswordDialog {
   def show(localization: Localization, simpleDatabaseIds: List[SimpleDatabaseId]): Option[LoginPasswords] = {
     val passwordFields = mkPasswordFields(simpleDatabaseIds.size)
-    val items = mkItems(localization, simpleDatabaseIds, passwordFields)
+    val titles = mkTitles(simpleDatabaseIds, localization)
     new Dialog[LoginPasswords]() {
-      title = s"${localization.password}"
-      dialogPane().content = new VBox { children = items }
+      title = s"${if(simpleDatabaseIds.size > 1) localization.passwords else localization.password}"
+      dialogPane().content = buildContent(passwordFields, titles)
       dialogPane().buttonTypes = Seq(ButtonType.OK, ButtonType.Cancel)
       resultConverter = dialogButton => convertResult(simpleDatabaseIds, passwordFields, dialogButton)
     }.showAndWait().asInstanceOf[Option[LoginPasswords]]
+  }
+
+  private def buildContent(passwordFields: List[PasswordField], titles: List[Label]): GridPane = {
+    val pane: GridPane = new GridPane {}
+    titles.zipWithIndex.foreach((title, row) => pane.add(title, 0, row))
+    passwordFields.zipWithIndex.foreach((passwordField, row) => pane.add(passwordField, 1, row))
+    pane
+  }
+
+  private def mkTitles(simpleDatabaseIds: List[SimpleDatabaseId], localization: Localization): List[Label] = {
+    simpleDatabaseIds.map(id => JFXUtil.buildTitle(s"${localization.database} ${id.databaseName}"))
   }
 
   private def mkPasswordFields(size: Int) : List[PasswordField] =
     List.range(0, size).map(_ => new PasswordField {
       text = ""
     })
-
-  private def mkItems(localization: Localization, simpleDatabaseIds: List[SimpleDatabaseId], passwordFields: List[PasswordField]) =
-    simpleDatabaseIds.zip(passwordFields).map(
-      (id, passwordField) => JFXUtil.withLeftTitle(passwordField, s"${localization.databases} ${id.databaseName}")
-    )
 
   private def convertResult(simpleDatabaseIds: List[SimpleDatabaseId], passwordFields: List[PasswordField], dialogButton: ButtonType): LoginPasswords = {
     if (dialogButton == ButtonType.OK)
