49b5d7b9f1bfe8adab16d71a38c03884b133f3b3
aferrandi <ferrandi,andrea@gmail.com>
2024-03-16 23:02:15 +0100

    removed the from table from the foreign key pattern matching

--- src/main/scala/dbtarzan/db/actor/ForeignKeysByPattern.scala
+++ src/main/scala/dbtarzan/db/actor/ForeignKeysByPattern.scala
@@ -8,14 +8,23 @@ object ForeignKeysByPattern {
     foreignKeys.filter(fk => foreignKeyContainsPattern(patternUppercase, fk))
   }
 
+  /*
+   looks for the pattern in:
+       - the name of the foreign key
+       - in  the to-table
+       - in the fields of the to-table included in the foreign key
+       - in the fields of the from-table included in the foreign key (the fields of the open table)
+   */
   private def foreignKeyContainsPattern(patternUppercase: String, fk: ForeignKey) = {
-    fk.name.toUpperCase().contains(patternUppercase) ||
+      fk.name.toUpperCase().contains(patternUppercase) ||
+      tableNameContainsPattern(fk.to, patternUppercase) ||
       fieldsOnTableContainsPattern(fk.to, patternUppercase) ||
       fieldsOnTableContainsPattern(fk.from, patternUppercase)
   }
 
-  private def fieldsOnTableContainsPattern(fieldsOnTable: FieldsOnTable, patternUppercase: String) = {
-    fieldsOnTable.table.tableName.toUpperCase().contains(patternUppercase) ||
-      fieldsOnTable.fields.exists(field => field.toUpperCase().contains(patternUppercase))
-  }
+  private def tableNameContainsPattern(fieldsOnTable: FieldsOnTable, patternUppercase: String) =
+    fieldsOnTable.table.tableName.toUpperCase().contains(patternUppercase)
+
+  private def fieldsOnTableContainsPattern(fieldsOnTable: FieldsOnTable, patternUppercase: String) =
+    fieldsOnTable.fields.exists(field => field.toUpperCase().contains(patternUppercase))
 }
