7fa54472e9b8e88b5e0b2c5a32240490905dcfdf
aferrandi <ferrandi,andrea@gmail.com>
2024-03-23 16:53:53 +0100

    simplified the push.sh script

--- mksnap/push.sh
+++ mksnap/push.sh
@@ -1,5 +1,4 @@
-#prj/dbtarzan> mksnap/push.sh $PWD 1.25 18
+#prj/dbtarzan> mksnap/push.sh $PWD 1.25
 ROOTDIR=$1
 VERSION=$2
-REVISION=$3
 snapcraft upload --release=stable $ROOTDIR/mksnap/dbtarzan_$VERSION.0_amd64.snap
