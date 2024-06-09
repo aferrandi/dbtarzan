b8ef48f5f92dda97bc2f1162a49bade98c735053
aferrandi <ferrandi,andrea@gmail.com>
2024-06-09 23:04:35 +0200

    #127: fixed test

--- src/test/scala/dbtarzan/config/actor/DatabaseInfoExtractorTest.scala
+++ src/test/scala/dbtarzan/config/actor/DatabaseInfoExtractorTest.scala
@@ -9,19 +9,20 @@ import org.scalatest.flatspec.AnyFlatSpec
 import scala.collection.immutable.HashSet
 
 class DatabaseInfoExtractorTest extends AnyFlatSpec {
-  "databases included completely in composites" should "not appear in databases infos" in {
-    val currentComposites: List[Composite] = buldComposites
-    val connectionData: ConnectionsDataMap = buldConnections
+  "databases included completely in all composites of which they are part" should "not appear in databases infos." in {
+    val currentComposites: List[Composite] = buldComposites()
+    val connectionData: ConnectionsDataMap = buldConnections()
 
     val infos = DatabaseInfoExtractor.extractDatabaseInfos(currentComposites, connectionData)
     val ids = infos.infos.map(info => DatabaseIdUtil.databaseIdFromInfo(info))
-    assert(3 === ids.size)
+    assert(4 === ids.size)
     assert(ids.contains(DatabaseId(Right(CompositeId("Orapost")))))
     assert(ids.contains(DatabaseId(Right(CompositeId("oramy")))))
     assert(ids.contains(DatabaseId(Left(SimpleDatabaseId("postgreSql")))))
+    assert(ids.contains(DatabaseId(Left(SimpleDatabaseId("oracle")))))
   }
 
-  private def buldConnections = {
+  private def buldConnections() = {
     val connectionData = new ConnectionsDataMap(List(
       ConnectionData("oracle.jar", "oracle", "DriverOracle", "jdbc://oracle", None, "giovanni", Some(Password("malagodi")), None, None, None, None, None, None, None),
       ConnectionData("mysql.jar", "mysql", "DriverMysql", "jdbc://mysql", None, "arturo", Some(Password("fedele")),None, None, None, None, None, None, None),
@@ -30,7 +31,7 @@ class DatabaseInfoExtractorTest extends AnyFlatSpec {
     connectionData
   }
 
-  private def buldComposites = {
+  private def buldComposites() = {
     val currentComposites = List(
       Composite(CompositeId("Orapost"), List(SimpleDatabaseId("oracle"), SimpleDatabaseId("postgreSql")), true),
       Composite(CompositeId("oramy"), List(SimpleDatabaseId("oracle"), SimpleDatabaseId("mysql")), false)
@@ -39,8 +40,8 @@ class DatabaseInfoExtractorTest extends AnyFlatSpec {
   }
 
   "getting the infos matching a pattern" should "work and is case insensistive" in {
-    val currentComposites: List[Composite] = buldComposites
-    val connectionData: ConnectionsDataMap = buldConnections
+    val currentComposites: List[Composite] = buldComposites()
+    val connectionData: ConnectionsDataMap = buldConnections()
 
     containsNames(DatabaseInfoExtractor.extractDatabaseInfosByPattern(currentComposites, connectionData, "ORA"), Set("oracle", "Orapost", "oramy"))
     containsNames(DatabaseInfoExtractor.extractDatabaseInfosByPattern(currentComposites, connectionData, "sQL"), Set("mysql", "postgreSql", "Orapost", "oramy"))
