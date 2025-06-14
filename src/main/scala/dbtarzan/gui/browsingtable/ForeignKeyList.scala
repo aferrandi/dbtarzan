2d848c559889865be709f0715c0476ca0b16f0c4
andrea <ferrandi.andrea@gmail.com>
2025-06-14 14:21:29 +0200

    use a table to show the foreign keys

--- src/main/scala/dbtarzan/gui/browsingtable/ForeignKeyList.scala
+++ src/main/scala/dbtarzan/gui/browsingtable/ForeignKeyList.scala
@@ -2,24 +2,73 @@ package dbtarzan.gui.browsingtable
 
 import dbtarzan.db.{ForeignKey, ForeignKeys}
 import dbtarzan.gui.interfaces.TControlBuilder
-import dbtarzan.gui.util.JFXUtil
+import dbtarzan.gui.util.{ForeignKeyIcons, JFXUtil, TableIdLabel}
 import dbtarzan.messages.TLogger
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.Parent
-import scalafx.scene.control.{ListView, Tooltip}
+import scalafx.scene.control.{ListView, TableCell, TableColumn, TableView, Tooltip}
+import dbtarzan.localization.Localization
+import scalafx.beans.property.{ObjectProperty, StringProperty}
+import scalafx.scene.image.{Image, ImageView}
+import scalafx.Includes.*
 
-/* if the table has 2 or more foreign keys to the same table, we want to give more information to the user, so that he can understand which one to use */ 
-case class ForeignKeyWithSharingCheck(key: ForeignKey, sharesToTable : Boolean)
 
 /**	foreign keys list */
-class ForeignKeyList(log: TLogger) extends TControlBuilder {
-  private val buffer = ObservableBuffer.empty[ForeignKeyWithSharingCheck]
-  private val list = new ListView[ForeignKeyWithSharingCheck](buffer) {
-      cellFactory =  (cell, value) => {
-        cell.tooltip.value = Tooltip(ForeignKeyText.buildTooltip(value.key))
-        cell.text.value = ForeignKeyText.buildText(value)
-      }
+class ForeignKeyList(localization : Localization, log: TLogger) extends TControlBuilder {
+  private val buffer = ObservableBuffer.empty[ForeignKey]
+  private val keyTable = buildTable()
+
+
+  /* builds table with the two columns (name and description) */
+  def buildTable(): TableView[ForeignKey] = new TableView[ForeignKey](buffer) {
+    columns ++= List(directionColumn(), tableToColumn(), tableFromFields(), tableToFields())
+    editable = false
+    columnResizePolicy = javafx.scene.control.TableView.CONSTRAINED_RESIZE_POLICY_FLEX_LAST_COLUMN
+  }
+
+  /* the column with the from table description  */
+  private def tableFromColumn() = new TableColumn[ForeignKey, String] {
+    text = localization.tableFrom
+    cellValueFactory = { x => new StringProperty(TableIdLabel.toLabel(x.value.from.table)) }
+    resizable = true
+  }
+
+  /* the column with the from table description  */
+  private def tableFromFields() = new TableColumn[ForeignKey, String] {
+    text = localization.columnsFrom
+    cellValueFactory = { x => new StringProperty(x.value.from.fields.mkString(",")) }
+    resizable = true
+  }
+
+  /* the column with the from table description  */
+  private def tableToColumn() = new TableColumn[ForeignKey, String] {
+    text = localization.tableTo
+    cellValueFactory = { x => new StringProperty(TableIdLabel.toLabel(x.value.to.table)) }
+    resizable = true
+  }
+
+  /* the column with the from table description  */
+  private def tableToFields() = new TableColumn[ForeignKey, String] {
+    text = localization.columnsTo
+    cellValueFactory = { x => new StringProperty(x.value.to.fields.mkString(",")) }
+    resizable = true
+  }
+
+  /* build the column on the left, that shows the icon (error, warn, info) */
+  private def directionColumn() = new TableColumn[ForeignKey, Image] {
+    cellValueFactory = { row => ObjectProperty(ForeignKeyIcons.iconForDirection(row.value.direction).delegate) }
+    cellFactory = {
+      (_: TableColumn[ForeignKey, Image]) =>
+        new TableCell[ForeignKey, Image] {
+          item.onChange {
+            (_, _, newImage) => graphic = new ImageView(newImage)
+          }
+        }
     }
+    maxWidth = 24
+    minWidth = 24
+  }
+
 
   /** need to show only the "to table" as cell text. And a tooltip for each cell	*/
   def addForeignKeys(newForeignKeys : ForeignKeys) : Unit = {
@@ -28,11 +77,9 @@ class ForeignKeyList(log: TLogger) extends TControlBuilder {
 
   private def setForeignKeys(newForeignKeys: ForeignKeys): Unit = {
     def moreThanOneItem(l: List[?]) = l.length > 1
-    log.debug("newForeignKeys " + newForeignKeys)
-    val allForeignKeys = buffer.toList.map(_.key) ++ newForeignKeys.keys
-    val groupedByToTableInsensitive = allForeignKeys.groupBy(_.to.table.tableName.toUpperCase()).values
-    val withSharingCheck = groupedByToTableInsensitive.flatMap(ks => ks.map(ForeignKeyWithSharingCheck(_, moreThanOneItem(ks))))
-    JFXUtil.bufferSet(buffer, withSharingCheck)
+    log.debug(s"newForeignKeys $newForeignKeys")
+    val allForeignKeys = buffer.toList ++ newForeignKeys.keys
+    JFXUtil.bufferSet(buffer, allForeignKeys)
   }
 
   def setForeignKeysByPattern(newForeignKeys : ForeignKeys) : Unit = {
@@ -42,10 +89,11 @@ class ForeignKeyList(log: TLogger) extends TControlBuilder {
 
   /* foreign key double-clicked. handled by BrowsingTable that has knowledge of tables too */
   def onForeignKeySelected(useKey : (ForeignKey, Boolean)  => Unit) : Unit =
-     JFXUtil.onAction(list, { (selectedKey : ForeignKeyWithSharingCheck, ctrlDown) =>
+     JFXUtil.onAction(keyTable, { (selectedKey : ForeignKey, ctrlDown) =>
         log.debug(s"Selected $selectedKey")
-        Option(selectedKey).foreach(k => useKey(k.key, ctrlDown))
+        Option(selectedKey).foreach(key => useKey(key, ctrlDown))
       })
-  def control : Parent = list
- }
+
+  def control : Parent = keyTable
+}
 
--- src/main/scala/dbtarzan/gui/browsingtable/ForeignKeyText.scala
+++ src/main/scala/dbtarzan/gui/browsingtable/ForeignKeyText.scala
@@ -5,15 +5,6 @@ import dbtarzan.gui.util.TableIdLabel
 
 object ForeignKeyText {
 
-  /** the text shows the table name, the direction ("<"" if the foreign key is straight, ">"" if it is turned)
-   * if there is more than one foreign key with the same "to" table, also the foreign key fields are displayed */
-  def buildText(key : ForeignKeyWithSharingCheck): String = {
-    val direction = directionText(key.key.direction)
-    val fields = fieldsIfSharesTable(key).map(t => " " + t).getOrElse("")
-    val table = TableIdLabel.toLabel(key.key.to.table)
-    s"$direction $table$fields"
-  }
-
   /** the tooltip show the whole foreign key */
   def buildTooltip(key: ForeignKey): String = {
     s"${key.name}\n- ${buildSide(key.from)}\n- ${buildSide(key.to)}"
@@ -21,9 +12,6 @@ object ForeignKeyText {
 
   private def buildSide(fields: FieldsOnTable): String =
     TableIdLabel.toLabel(fields.table) + fieldsToText(fields.fields)
-
-  private def fieldsIfSharesTable(key : ForeignKeyWithSharingCheck): Option[String] =
-    Some(key).filter(_.sharesToTable).map(k => fieldsToText(k.key.from.fields))
 
   private def fieldsToText(fields: List[String]): String = fields.mkString("(", ",", ")")
 
